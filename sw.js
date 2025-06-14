eacd81373310cf3e710d835ac85ce872d5a7ed6d
undercover17 <159145033+undercover17@users.noreply.github.com>
2025-06-14 13:10:18 +0700

    sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('pwa-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
